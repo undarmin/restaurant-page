50535ef3d9347c6f0b2cc1547799af2b3c495f9a
undarmin <tejasondg@Gmail.com>
2024-02-29 19:48:33 +0530

    Import style.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 import { createHome } from "./home.js";
+import { createMenu } from "./menu.js";
+import { createAbout } from "./about.js";
+import './style.css';
 
+const homeButton = document.querySelector("#home");
+const menuButton = document.querySelector("#menu");
+const aboutButton = document.querySelector("#about");
 const content = document.querySelector("div#content");
-console.log(content.children)
+
+function displayContent(fxn) {
+  [...content.children].forEach((child) => {
+    content.removeChild(content.children[[...content.children].indexOf(child)]);
+  });
+  fxn(content);
+}
+
+homeButton.addEventListener("click", () => {
+  displayContent(createHome);
+});
+
+menuButton.addEventListener("click", () => {
+  displayContent(createMenu);
+});
+
+aboutButton.addEventListener("click", () => {
+  displayContent(createAbout);
+});
+
 createHome(content);
