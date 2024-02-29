fe80bfed200db237ddeb940beafd5f86772a6fa1
undarmin <tejasondg@Gmail.com>
2024-02-29 13:09:30 +0530

    Get function to create content in dist/index.html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("Webpack easing");
+import { createHome } from "./home.js";
+
+const content = document.querySelector("div#content");
+console.log(content.children)
+createHome(content);
